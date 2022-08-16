a7818fd551c24191af74b0ea528ed1a6dbbdc4ff
Damian Davila <damian@moventisusa.com>
2022-08-16 15:09:07 -0400

    Exercises 3.13-3.14

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
+const Person = require('./models/person');
 
 app.use(express.json());
 app.use(cors());
@@ -48,13 +50,16 @@ app.get("/info/", (request, response) => {
 });
 
 app.get("/api/persons/", (request, response) => {
-  response.json(persons);
+  Person.find({}).then(persons => {
+      response.json(persons);
+    })
 });
 
 app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  response.json(person);
+  Person.findById(id).then(person => {
+    response.json(person);  
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -84,21 +89,24 @@ app.post("/api/persons/", (request, response) => {
             error: 'phone missing'
         });
     }
+    /*
     const nameExists = persons.find(person => person.name.toLowerCase() === body.name.toLowerCase());
     if (nameExists) {
         return response.status(400).json({
             error: 'name must be unique'
         })
     }
+    */
 
-    const maxId = 10000000;
-    const person = {
-        "id" : generateId(),
-        "name" : body.name,
-        "phone" : body.phone
-    }
-    persons = persons.concat(person)
-    response.json(person);
+    const person = new Person({
+        name : body.name,
+        phone : body.phone
+    });
+    //persons = persons.concat(person)
+    person.save().then(savedPerson => {
+      response.json(savedPerson);
+    })
+    
   });
   
 const PORT = process.env.PORT || 3001;
---
+++ models/person.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const url = process.env.MONGODB_URI;
+// URI saved in .env file locally, and Config Vars on Heroku for deployment
+// `mongodb+srv://fsopen:${password}@cluster0.rpt6zx8.mongodb.net/phoneBookApp?retryWrites=true&w=majority`;
+
+console.log('connecting to', url);
+
+mongoose
+    .connect(url)
+    .then((result) => {
+        console.log("connected to MongoDB");
+    })
+    .catch((error) => {
+        console.log("error connecting to MongoDB:", error.message);
+    })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  phone: String,
+});
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+
+const Person = mongoose.model("Person", personSchema);
+
+module.exports = mongoose.model('Person', personSchema);
